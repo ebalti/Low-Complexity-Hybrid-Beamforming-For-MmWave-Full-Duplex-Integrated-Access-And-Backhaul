9df9670f18e262466bf414483e11e66781b12969
Elyes Balti <ebalti@utexas.edu>
2023-02-17 11:03:28 -0600

    Add files via upload

---
+++ citation.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% citation.m 
+%
+% Created Feb, 2023
+% Elyes Balti
+% The University of Texas at Austin
+%
+% If you use this code or any (modified) part of it in any publication, please cite 
+% the following paper: 
+% 
+% E. Balti, C. Dick and B. L. Evans, "Low Complexity Hybrid Beamforming for mmWave Full-Duplex
+% Integrated Access and Backhaul," GLOBECOM 2022 - 2022 IEEE Global Communications Conference, 
+% Rio de Janeiro, Brazil, 2022, pp. 1606-1611, doi: 10.1109/GLOBECOM48099.2022.10001634.
+%
+%  BibTex Citation
+%
+%  @INPROCEEDINGS{10001634,
+%  author={Balti, Elyes and Dick, Chris and Evans, Brian L.},
+%  booktitle={GLOBECOM 2022 - 2022 IEEE Global Communications Conference}, 
+%  title={Low Complexity Hybrid Beamforming for mmWave Full-Duplex Integrated Access and Backhaul}, 
+%  year={2022},
+%  volume={},
+%  number={},
+%  pages={1606-1611},
+%  doi={10.1109/GLOBECOM48099.2022.10001634}}
+%
+% Contact email: ebalti@utexas.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
